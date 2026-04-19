babee9257d50071923149f4797eeaa5395b2dd2b
LmeoHme <luatnguyengia00@gmail.com>
2026-04-19 10:33:40 +0900

    Finish 11_tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FDegree) {
+  let CDegree = (FDegree - 32) / (9 / 5);
+  return Math.round((CDegree + Number.EPSILON) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CDegree) {
+  let FDegree = CDegree * (9 / 5) + 32;
+  return Math.round((FDegree + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line

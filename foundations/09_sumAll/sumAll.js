1108dddc9f83eddbb94f61388a5fee9877346199
LmeoHme <luatnguyengia00@gmail.com>
2026-04-19 09:48:29 +0900

    Finish 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+function isPositiveInt(number)
+{
+    if (isFinite(number))
+    {
+        if (Number.isInteger(number) && number > 0) return true;
+    }
+    else return false;
+}
 
+const sumAll = function(startNum, endNum) {
+    if (!(isPositiveInt(startNum) && isPositiveInt(endNum))) return "ERROR";
+
+    // return ((Math.abs(endNum - startNum) + 1) * (startNum + endNum)) / 2;
+
+        if (startNum > endNum)
+    {
+        let temp = startNum;
+        startNum = endNum;
+        endNum = temp;    
+    }
+    const numbers = [];
+    for (startNum; startNum <= endNum;)
+    {
+        numbers.push(startNum++);
+    }
+    return numbers.reduce((sum, num) => sum += num, 0);
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

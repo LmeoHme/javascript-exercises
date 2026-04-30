f1f1b8d8016877418304162bfdd5cd0345adbc33
LmeoHme <luatnguyengia00@gmail.com>
2026-04-30 09:40:47 +0900

    Finish 14_fibonacci exercise.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (!validateInput(number)) return "OOPS";
+    if (+number === 0) return 0;
+    else if (+number === 1) return 1;
+    else return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
+function validateInput(input)
+{
+    if (isFinite(input) && input >= 0) return true;
+    else return false;
+}
 // Do not edit below this line
 module.exports = fibonacci;

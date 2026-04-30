2a93393d34e73095f5d1e663f83c3fb0effa1447
LmeoHme <luatnguyengia00@gmail.com>
2026-04-30 09:06:20 +0900

    Finish 13_palindromes exercise.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = normalizeString(string);
+    const charArray = string.split("");
+    let i = 0;
+    let j = charArray.length - 1;
+    let midIndex = Math.round(charArray.length / 2);
+    let isRunning = true;
+    while(isRunning)
+    {
+        if (charArray[i] !== charArray[j] || i - j > 0) return false;
+        if (i === j) return true;
+        i++;
+        j--;
+    }
 };
 
+function normalizeString(string)
+{
+  let specialChars = /[^a-zA-Z0-9]/g;
+  return string.toLowerCase().replace(specialChars, "");
+}
 // Do not edit below this line
 module.exports = palindromes;

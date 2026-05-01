d1be449c370f934f2ec3e3d4674383531e7723a7
LmeoHme <luatnguyengia00@gmail.com>
2026-05-01 14:12:29 +0900

    Finish 16_findTheOldest exercise.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (person["yearOfDeath"] === undefined) person.age = (new Date()).getFullYear() - person.yearOfBirth;
+        else person.age = person.yearOfDeath - person.yearOfBirth;
+    });
+    people.sort((a, b) => b.age - a.age);
+    return people[0];
 };
 
 // Do not edit below this line
